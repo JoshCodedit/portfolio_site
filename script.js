99af7b568a960d9b842f4ef8b9cda697a8a79265
JoshCodedit <joshua.dixoncms@gmail.com>
2024-04-25 18:41:41 +0100

    updates site

--- script.js
+++ script.js
@@ -1,53 +1,53 @@
 
-                document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {
 
-            const testimonial1 = document.querySelector('.testimonial-1');
-            const testimonial2 = document.querySelector('.testimonial-2');
-            const testimonial3 = document.querySelector('.testimonial-3');
-            const testimonial4 = document.querySelector('.testimonial-4');
-            const nextPage = document.querySelector('#togglePage1');
-            const nextPage2 = document.querySelector('#togglePage2');
-            const prevPage = document.querySelector('#togglePage3');
-            const prevPage2 = document.querySelector('#togglePage4')
-    
+    const testimonial1 = document.querySelector('.testimonial-1');
+    const testimonial2 = document.querySelector('.testimonial-2');
+    const testimonial3 = document.querySelector('.testimonial-3');
+    const testimonial4 = document.querySelector('.testimonial-4');
+    const nextPage = document.querySelector('#togglePage1');
+    const nextPage2 = document.querySelector('#togglePage2');
+    const prevPage = document.querySelector('#togglePage3');
+    const prevPage2 = document.querySelector('#togglePage4')
 
+
+    testimonial3.style.display = 'none';
+    testimonial4.style.display = 'none';
+    prevPage.style.display = 'none';
+    prevPage2.style.display = 'none';
+
+
+    nextPage.addEventListener('click', function () {
+            testimonial1.style.display = 'none';
+            testimonial3.style.display = '';
+            nextPage.style.display = 'none';
+            prevPage.style.display = '';
+
+    });
+    prevPage.addEventListener('click', function () {
+
+            testimonial1.style.display = '';
             testimonial3.style.display = 'none';
-            testimonial4.style.display = 'none';
+            nextPage.style.display = '';
             prevPage.style.display = 'none';
+
+    });
+
+
+    nextPage2.addEventListener('click', function () {
+
+            testimonial2.style.display = 'none';
+            testimonial4.style.display = '';
+            nextPage2.style.display = 'none';
+            prevPage2.style.display = '';
+
+    });
+    prevPage2.addEventListener('click', function () {
+
+            testimonial2.style.display = '';
+            testimonial4.style.display = 'none';
+            nextPage2.style.display = '';
             prevPage2.style.display = 'none';
 
-
-            nextPage.addEventListener('click', function () {
-                    testimonial1.style.display = 'none';
-                    testimonial3.style.display = '';
-                    nextPage.style.display = 'none';
-                    prevPage.style.display = '';
-
-            });
-            prevPage.addEventListener('click', function () {
-
-                    testimonial1.style.display = '';
-                    testimonial3.style.display = 'none';
-                    nextPage.style.display = '';
-                    prevPage.style.display = 'none';
-
-            });
-            
-
-            nextPage2.addEventListener('click', function () {
-
-                    testimonial2.style.display = 'none';
-                    testimonial4.style.display = '';
-                    nextPage2.style.display = 'none';
-                    prevPage2.style.display = '';
-
-            });
-            prevPage2.addEventListener('click', function () {
-
-                    testimonial2.style.display = '';
-                    testimonial4.style.display = 'none';
-                    nextPage2.style.display = '';
-                    prevPage2.style.display = 'none';
-
-            });
-        });
+    });
+});
